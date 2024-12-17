99c991324c280fccdd2119197bb0d69c58a49811
Zikriiiww <zikriakspen@gmail.com>
2024-12-17 09:59:07 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,7 +1,48 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int x;
-  cout << "Hello" << endl;
+  #include <iostream>
+using namespace std;
+
+struct pesawat
+{
+    string nama_pesawat;
+    string jenis;
+    double harga_pesawat;
+    string tanggal_pesawat;
+    string kursi_pesawat;
+};
+int main(){
+    int menu;
+    cout << "Loading......\n";
+    cout <<"Opening the program\n";
+    for (int i = 0; i <= menu; i++)
+    {   
+    cout << "Silakan piih menu di bawah ini" << endl;
+    cout << "1. Lihat Tiket\n"; 
+    cout << "2. Pesanan saya\n";
+    cout << "3. Check in\n";
+    cin >> menu;
+    if (menu==1){
+        cout << "Lihat tiket\n";
+        cin >> menu;
+        if (menu==1){
+            cout << "Filter berdasarkan harga termahal\n";
+        } else if (menu==2)
+        {
+            cout << "Filter berdasarkan harga termurah\n";
+        } else{
+            break;
+        }
+        
+    }
+    }
+   pesawat Lion;
+   Lion.jenis="Lion air";
+   cout << Lion.jenis<< endl;
+    
+
+    cout << "Terimakasih";
+}
 return 0;
 }
